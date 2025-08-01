22744e49ea35c352ed74ed7f7eadd4af4e833ea6
Thorvald <thorvaldmortele@hotmail.be>
2025-08-01 22:37:57 +0200

    [ADD] added dictionairy + word validity check [MOD] tweaked circle words shifting

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,20 @@
+using System.Collections;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public WordSlotManager wordSlotManager;
+    public DictionaryLoader DictionaryLoader;
 
     void Start()
     {
+        StartCoroutine(LoadWords());
+    }
+
+    private IEnumerator LoadWords()
+    {
+        yield return StartCoroutine(DictionaryLoader.LoadWordsCoroutine());
+
         wordSlotManager.AddWord("apple");
     }
 
--- Assets/Scripts/WordSlotManager.cs
+++ Assets/Scripts/WordSlotManager.cs
@@ -1,4 +1,129 @@
+//using System.Collections.Generic;
+//using DG.Tweening;
+//using TMPro;
+//using UnityEngine;
+
+//public class WordSlotManager : MonoBehaviour
+//{
+//    public GameObject wordPrefab;
+//    public Transform wordParent;
+
+//    public float baseRadius = 200f;
+//    public float degreesPerLetter = 10f;
+//    public float wordSpacingDegrees = 5f;
+//    public float inputAngle = 0f; // Input is at 0°, so first word goes at 180°
+
+//    private List<CurvedWordDisplay> curvedWords = new();
+
+//    public int wordCount => curvedWords.Count;
+
+//    public void AddWord(string word)
+//    {
+//        var wordObj = Instantiate(wordPrefab, wordParent);
+//        var curved = wordObj.GetComponent<CurvedWordDisplay>();
+//        curved.radius = baseRadius;
+//        curved.degreesPerLetter = degreesPerLetter;
+//        curved.SetWord(word, 0); // Temp angle
+
+//        curvedWords.Insert(0, curved);
+
+//        float angleCursor = 180f;
+//        for (int i = 0; i < curvedWords.Count; i++)
+//        {
+//            var wordDisplay = curvedWords[i];
+//            float span = wordDisplay.GetLetterCount() * degreesPerLetter;
+
+//            if (i == 0)
+//            {
+//                AnimateToAngle(wordDisplay, angleCursor);
+//            }
+//            else
+//            {
+//                float prevSpan = curvedWords[i - 1].GetLetterCount() * degreesPerLetter;
+//                float shift = (prevSpan + span) / 2f + wordSpacingDegrees;
+//                angleCursor += shift;
+
+//                AnimateToAngle(wordDisplay, angleCursor);
+//            }
+//        }
+//    }
+
+//    private void AnimateToAngle(CurvedWordDisplay display, float targetAngle)
+//    {
+//        DOTween.To(
+//            () => display.centerAngle,
+//            a =>
+//            {
+//                display.centerAngle = a;
+//                display.ApplyCurve();
+//            },
+//            targetAngle,
+//            0.3f
+//        ).SetEase(Ease.OutQuad);
+//    }
+//    public string GetLastLetter()
+//    {
+//        if (curvedWords.Count == 0) return "";
+//        string latestWord = curvedWords[0].GetComponent<TextMeshProUGUI>().text.Trim();
+//        return latestWord.Length > 0 ? latestWord[^1].ToString().ToLower() : "";
+//    }
+//    public string GetLastWord()
+//    {
+//        return curvedWords.Count == 0 ? "" : curvedWords[0].GetComponent<TextMeshProUGUI>().text;
+//    }
+//}
+
+//using System.Collections.Generic;
+//using DG.Tweening;
+//using TMPro;
+//using UnityEngine;
+
+//public class WordSlotManager : MonoBehaviour
+//{
+//    public GameObject wordPrefab;
+//    public Transform wordParent;
+//    public float baseRadius = 200f;
+//    public float wordSpacingDegrees = 5f;
+
+//    private List<CurvedWordDisplay> curvedWords = new();
+//    public int wordCount => curvedWords.Count;
+
+//    public void AddWord(string word)
+//    {
+//        var wordObj = Instantiate(wordPrefab, wordParent);
+//        var curved = wordObj.GetComponent<CurvedWordDisplay>();
+//        curved.radius = baseRadius;
+//        curved.SetWord(word, 0); // Temp angle
+
+//        curvedWords.Insert(0, curved);
+
+//        // Start placing from input angle (0°), go counterclockwise
+//        float angleCursor = 0f;
+//        for (int i = 0; i < curvedWords.Count; i++)
+//        {
+//            var wordDisplay = curvedWords[i];
+//            float arc = wordDisplay.GetArcAngle();
+//            angleCursor -= arc / 2f;
+//            AnimateToAngle(wordDisplay, angleCursor);
+//            angleCursor -= arc / 2f + wordSpacingDegrees;
+//        }
+//    }
+
+//    private void AnimateToAngle(CurvedWordDisplay display, float angle)
+//    {
+//        DOTween.To(() => display.GetAngle(), display.SetAngle, angle, 0.3f);
+//    }
+
+//    public string GetLastLetter()
+//    {
+//        if (curvedWords.Count == 0) return "";
+//        string latestWord = curvedWords[0].GetComponent<TextMeshProUGUI>().text.Trim();
+//        return latestWord.Length > 0 ? latestWord[^1].ToString().ToLower() : "";
+//    }
+//}
+
 using System.Collections.Generic;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
@@ -6,64 +131,42 @@ public class WordSlotManager : MonoBehaviour
 {
     public GameObject wordPrefab;
     public Transform wordParent;
-
     public float baseRadius = 200f;
-    public float spacingDegrees = 30f;
-    public int maxWords = 10;
+    public float wordSpacingDegrees = 5f;
 
-    public int wordCount => wordObjects.Count;
-
-    private List<GameObject> wordObjects = new();
+    private List<CurvedWordDisplay> curvedWords = new();
+    public int wordCount => curvedWords.Count;
 
     public void AddWord(string word)
     {
-        if (wordObjects.Count >= maxWords)
+        var wordObj = Instantiate(wordPrefab, wordParent);
+        var curved = wordObj.GetComponent<CurvedWordDisplay>();
+        curved.radius = baseRadius;
+        curved.SetWord(word, 0); // Temp angle
+
+        curvedWords.Insert(0, curved);
+
+        // Start placing from 180° (left of input field), go counterclockwise
+        float angleCursor = 130f;
+        for (int i = 0; i < curvedWords.Count; i++)
         {
-            Destroy(wordObjects[0]);
-            wordObjects.RemoveAt(0);
+            var wordDisplay = curvedWords[i];
+            float arc = wordDisplay.GetArcAngle();
+            angleCursor += arc / 2f;
+            AnimateToAngle(wordDisplay, angleCursor);
+            angleCursor += arc / 2f + wordSpacingDegrees;
         }
-
-        GameObject obj = Instantiate(wordPrefab, wordParent);
-        CurvedWordDisplay display = obj.GetComponent<CurvedWordDisplay>();
-
-        float angle = wordObjects.Count * -spacingDegrees;
-        display.radius = baseRadius;
-        display.degreesPerLetter = 10f;
-        display.SetWord(word, angle);
-
-        wordObjects.Add(obj);
-        UpdateWordPositions();
     }
 
-    private void UpdateWordPositions()
+    private void AnimateToAngle(CurvedWordDisplay display, float angle)
     {
-        for (int i = 0; i < wordObjects.Count; i++)
-        {
-            var display = wordObjects[i].GetComponent<CurvedWordDisplay>();
-            if (display == null) continue;
-
-            float angle = i * -spacingDegrees;
-            string word = display.GetComponent<TextMeshProUGUI>().text;
-            display.SetWord(word, angle);
-        }
+        DOTween.To(() => display.GetAngle(), display.SetAngle, angle, 0.3f);
     }
 
     public string GetLastLetter()
     {
-        if (wordObjects.Count == 0) return "";
-
-        var display = wordObjects[^1].GetComponent<TextMeshProUGUI>();
-        if (display == null) return "";
-
-        string lastWord = display.text.Trim();
-        if (string.IsNullOrEmpty(lastWord)) return "";
-
-        return lastWord[^1].ToString().ToLower();
-    }
-
-    public string GetLastWord()
-    {
-        if (wordObjects.Count == 0) return "";
-        return wordObjects[^1].GetComponent<TextMeshProUGUI>().text;
+        if (curvedWords.Count == 0) return "";
+        string latestWord = curvedWords[0].GetComponent<TextMeshProUGUI>().text.Trim();
+        return latestWord.Length > 0 ? latestWord[^1].ToString().ToLower() : "";
     }
 }
