d0904444f354dc751c55bd5618b0fda7460ed688
Vincent <vincent@cyberneticwalrus.com>
2025-08-02 21:04:11 +0200

    Added UI sounds WIP

---
+++ Assets/Scripts/UISoundManager.cs
@@ -0,0 +1,32 @@
+using FMODUnity;
+using UnityEngine;
+
+public class UISoundManager : MonoBehaviour
+{
+
+    public EventReference hoverSFX;
+    public EventReference pressedSFX;
+
+
+    public void UIHoverSFX()
+    {
+        var instance = RuntimeManager.CreateInstance(hoverSFX.Guid);
+
+        instance.set3DAttributes(RuntimeUtils.To3DAttributes(this.transform));
+        instance.start();
+        instance.release();
+    }
+
+    public void UIPressedSFX()
+    {
+        var instance = RuntimeManager.CreateInstance(pressedSFX.Guid);
+
+        instance.set3DAttributes(RuntimeUtils.To3DAttributes(this.transform));
+        instance.start();
+        instance.release();
+    }
+
+
+
+
+}
